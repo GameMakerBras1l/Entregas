67f00376e084696009b8500b52a4b59b26018f0e
GameMakerBras1l <marcelciro2015@gmail.com>
2025-06-11 16:01:51 -0300

    Novos robos criados

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 
 class Robo:
     # Construtor da classe
-    def __init__(self):
-        id = ""
-        capCargaKg = ""
+    def __init__(self, id, capCargaKg):
+        self.id = id
+        self.capCargaKg = capCargaKg
+
+
+
+RoboLeve = Robo(str("RB-01"), int(10))
+
+RoboPesado = Robo(str("RB-02"), int(50))
