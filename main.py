f355b3a5ba8c56a492eaf7fe809b7847a0beca32
GameMakerBras1l <marcelciro2015@gmail.com>
2025-06-11 16:13:16 -0300

    Nova classe criada

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ class Robo:
         self.capCargaKg = capCargaKg
 
 
-
 RoboLeve = Robo(str("RB-01"), int(10))
 
 RoboPesado = Robo(str("RB-02"), int(50))
+
+
+class Pacote:
+    def __init__(self, conteudo, pesoKg, descricao, roboRespon):
+        self.conteudo = conteudo
+        self.pesoKg = pesoKg
+        self.descricao = descricao
+        self.roboRespon = roboRespon
