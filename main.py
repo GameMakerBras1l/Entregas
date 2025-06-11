b5ab52fa75c763e78182f6608c26ee826b3017fc
GameMakerBras1l <marcelciro2015@gmail.com>
2025-06-11 15:47:26 -0300

    Classe criada

---
+++ main.py
@@ -0,0 +1,7 @@
+# Passo 1: Criar classe robo
+
+class Robo:
+    # Construtor da classe
+    def __init__(self):
+        id = ""
+        capCargaKg = ""
--- robos.py
+++
@@ -1,6 +0,0 @@
-# Passo 1: Criar classe robo
-
-class Robo:
-    # Construtor da classe
-    def entrega(self):
-        print("O robo esta realizando uma entrega")
