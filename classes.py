97d6721ed942e14f63254e0f7694373b1956f2c7
GameMakerBras1l <marcelciro2015@gmail.com>
2025-06-12 19:30:30 -0300

    Codigo prepararado mas incompleto ainda

--- classes.py
+++ classes.py
@@ -1,13 +1,18 @@
 # Passo 1: criar classes para robos e pacotes
 # Passo 2: criar um sistema de entergas usando essas classes
 
+class Pacotes:
+    def __init__(self, conteudo, pesoKg):
+        self.conteudo = conteudo
+        self.pesoKg = pesoKg
+
 class Robos:
     def __init__(self, idRobo, capKg):
         self.idRobo = idRobo
         self.capKg = capKg
 
-    def entregar_pacote(self):
-        if self.capKg >= 15:
+    def entregar_pacote(self, pacote):
+        if self.capKg >= pacote.pesoKg:
             print("Entrega realizada")
         else:
             print("Entrega negada")
--- encomenda.py
+++ encomenda.py
@@ -1,7 +1,14 @@
+from classes import Pacotes
 from classes import Robos
 
+produto = input("Informe o produto desejado: ")
+peso = int(input("Informe o peso do produto: "))
+
+pacote = Pacotes(produto, peso)
+
+
 roboLeve = Robos("R1", 10)
-roboLeve.entregar_pacote()
+roboLeve.entregar_pacote(pacote)
 
 roboPesado = Robos("R2", 50)
-roboPesado.entregar_pacote()
+roboPesado.entregar_pacote(pacote)
