f7ad2749353f8c3426688d409367ef62099b5c9b
GameMakerBras1l <marcelciro2015@gmail.com>
2025-06-13 20:41:40 -0300

    Atualizaaçãao

--- encomenda.py
+++ encomenda.py
@@ -5,10 +5,11 @@ produto = input("Informe o produto desejado: ")
 peso = int(input("Informe o peso do produto: "))
 
 pacote = Pacotes(produto, peso)
-
-
-roboLeve = Robos("R1", 10)
-roboLeve.entregar_pacote(pacote)
-
-roboPesado = Robos("R2", 50)
-roboPesado.entregar_pacote(pacote)
+if peso <= 10:
+    roboLeve = Robos("R1", 10)
+    roboLeve.entregar_pacote(pacote)
+elif 10 < peso <= 50:
+    roboPesado = Robos("R2", 50)
+    roboPesado.entregar_pacote(pacote)
+else:
+    print("Falha na entrega")
