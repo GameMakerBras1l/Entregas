dfb6d49408e908ce4834e89d09ae95fa370d616d
GameMakerBras1l <marcelciro2015@gmail.com>
2025-06-13 21:16:59 -0300

    Upgrade

--- classes.py
+++ classes.py
@@ -2,9 +2,14 @@
 # Passo 2: criar um sistema de entergas usando essas classes
 
 class Pacotes:
-    def __init__(self, conteudo, pesoKg):
+    def __init__(self, conteudo, pesoKg, numProdutos):
         self.conteudo = conteudo
         self.pesoKg = pesoKg
+        self.numProdutos = numProdutos
+        self.pesoTotal = self.numProdutos * self.pesoKg
+
+    def num_produtos(self):
+        return self.pesoTotal
 
 class Robos:
     def __init__(self, idRobo, capKg):
@@ -12,5 +17,5 @@ class Robos:
         self.capKg = capKg
 
     def entregar_pacote(self, pacote):
-        if self.capKg >= pacote.pesoKg:
+        if self.capKg >= pacote.pesoTotal:
             print("Entrega encaminhada")
--- encomenda.py
+++ encomenda.py
@@ -1,15 +1,24 @@
 from classes import Pacotes
 from classes import Robos
 
+
+
 produto = input("Informe o produto desejado: ")
 peso = int(input("Informe o peso do produto: "))
+numero = int(input("Informe a quaantidade desse produto: "))
 
-pacote = Pacotes(produto, peso)
-if peso <= 10:
+
+
+pacote = Pacotes(produto, peso, numero)
+pesoEntrega = pacote.pesoTotal
+
+if pesoEntrega <= 10:
     roboLeve = Robos("R1", 10)
     roboLeve.entregar_pacote(pacote)
-elif 10 < peso <= 50:
+
+elif 10 < pesoEntrega <= 50:
     roboPesado = Robos("R2", 50)
     roboPesado.entregar_pacote(pacote)
+
 else:
     print("Excesso de peso, falha no encaminhamento do produto")
